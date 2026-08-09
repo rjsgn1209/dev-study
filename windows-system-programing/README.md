8481f7d5bf8c404efd9674b3be4f32dab48b20b1
rjsgn1209 <huu1209@naver.com>
2026-08-09 23:57:10 +0900

    study: Windows User Mode와 Kernel Mode 학습

---
+++ windows-system-programing/README.md
@@ -0,0 +1,3 @@
+# Windows System Programming
+
+Windows 운영체제의 시스템 프로그래밍을 학습하고 정리한다.
---
+++ windows-system-programing/basic/CreateProcessSample/CreateProcessSample/CreateProcessSample.cpp
@@ -0,0 +1,39 @@
+﻿// CreateProcessSample.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+#include <Windows.h>
+#include <lm.h>
+#include <Sddl.h>
+#include <Psapi.h>
+
+void CreateSample01()
+{
+	STARTUPINFO si;
+	::ZeroMemory(&si, sizeof(STARTUPINFO));
+
+	PROCESS_INFORMATION pi;
+	::ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
+
+	TCHAR szBuffer[_MAX_FNAME] = TEXT("notepad.exe");
+	if (::CreateProcess(NULL,
+		szBuffer,
+		NULL,
+		NULL,
+		FALSE,
+		0,
+		NULL,
+		NULL,
+		&si,
+		&pi))
+	{
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	}
+}
+
+int main()
+{
+	CreateSample01();
+}
