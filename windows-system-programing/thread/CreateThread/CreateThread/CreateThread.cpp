82ddde046dc984b0b33c2423c65a4e3dc45edabc
rjsgn1209 <huu1209@naver.com>
2026-08-10 23:17:00 +0900

    study: Thread 기본개념 및 CreateThread 코드 학습

---
+++ windows-system-programing/thread/CreateThread/CreateThread/CreateThread.cpp
@@ -0,0 +1,37 @@
+﻿// CreateThread.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <windows.h>
+
+DWORD WINAPI ThreadFunction(LPVOID pParam)
+{
+	std::cout << "ThreadFunction() - Begin" << std::endl;
+	std::cout << (const char*)pParam << std::endl;
+	std::cout << "ThreadFunction() - End" << std::endl;
+	return 0;
+}
+
+int main()
+{
+	std::cout << "main() - Begin" << std::endl;
+	DWORD dwThreadId = 0;
+	HANDLE hThread = ::CreateThread(
+		NULL,
+		0,
+		ThreadFunction,
+		(LPVOID)"PARAM",
+		0,
+		&dwThreadId);
+	if (hThread == NULL) {
+
+	}
+
+	std::cout << "main() - WaitForSingleObject() - before" << std::endl;
+
+	::WaitForSingleObject(hThread, INFINITE);
+	::CloseHandle(hThread);
+
+	std::cout << "main() - End" << std::endl;
+	return 0;
+}
