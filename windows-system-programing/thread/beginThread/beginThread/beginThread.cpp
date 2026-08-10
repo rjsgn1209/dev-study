63b150a55705bf698b4bae75ad85ecb8af7ec70d
rjsgn1209 <huu1209@naver.com>
2026-08-11 01:11:53 +0900

    study: Thread 컨트롤 학습

---
+++ windows-system-programing/thread/beginThread/beginThread/beginThread.cpp
@@ -0,0 +1,36 @@
+﻿// beginThread.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <process.h>
+#include <windows.h>
+
+UINT WINAPI ThreadFunction(LPVOID pParam)
+{
+    std::cout << "ThreadFunction() - Begin" << std::endl;
+    std::cout << (const char*)pParam << std::endl;
+    std::cout << "ThreadFunction() - End" << std::endl;
+    return 0;
+}
+
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunction,
+        (LPVOID)"PARAM",
+        0,
+        &nThreadId);
+    if (hThread == NULL) {
+    }
+
+    ::WaitForSingleObject(hThread, INFINITE);
+    ::CloseHandle(hThread);
+
+    std::cout << "main() - End" << std::endl;
+    return 0;
+}
