7a759347cb2569f2df2619794ec07bad9de11f6a
rjsgn1209 <huu1209@naver.com>
2026-08-10 20:25:29 +0900

    study: CreateProcess 코드 학습

--- windows-system-programing/process/CreateProcessSample/CreateProcessSample/CreateProcessSample.cpp
+++ windows-system-programing/process/CreateProcessSample/CreateProcessSample/CreateProcessSample.cpp
@@ -33,7 +33,47 @@ void CreateSample01()
 	}
 }
 
+void CreateSample02()
+{
+	PROCESS_INFORMATION pi;
+	::ZeroMemory(&pi, sizeof(pi));
+
+	STARTUPINFO si;
+	::ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(STARTUPINFO);	// CreateProcessAsUser 시 cb값이 STARUPINFO 사이즈가 맞는지 확인을 통해 정상 초기화된건지 판단함
+	si.lpDesktop = (LPWSTR)TEXT("winsta0\\default");	// 프로세스가 생성될 윈도우 스테이션(winsta0)과 데스크톱(default)을 지정
+	HANDLE hToken = NULL;
+	HANDLE hProcess = NULL;
+
+	hProcess = OpenProcess(PROCESS_QUERY_INFORMATION,
+		TRUE, ::GetCurrentProcessId());
+	::OpenProcessToken(hProcess, TOKEN_ALL_ACCESS, &hToken);
+
+	TCHAR szBuffer[_MAX_FNAME] = TEXT("notepad.exe");
+	if (CreateProcessAsUser(
+		hToken,
+		NULL,
+		szBuffer,
+		NULL,
+		NULL,
+		TRUE,
+		CREATE_NEW_CONSOLE,
+		NULL,
+		NULL,
+		&si,
+		&pi
+	))
+	{
+		::CloseHandle(pi.hProcess);
+		::CloseHandle(pi.hThread);
+	}
+
+	::CloseHandle(hToken);
+	::CloseHandle(hProcess);
+}
+
 int main()
 {
-	CreateSample01();
+	//CreateSample01();
+	CreateSample02();
 }
