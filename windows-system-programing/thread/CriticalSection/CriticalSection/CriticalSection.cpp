b44fa31344b606b41a2dece8d47b9bfe4b0d7bff
rjsgn1209 <huu1209@naver.com>
2026-08-14 01:40:49 +0900

    study: 스레드 동기화 학습
    Event, CriticalSection 객체 활용 예제 학습 및 개념 정리

---
+++ windows-system-programing/thread/CriticalSection/CriticalSection/CriticalSection.cpp
@@ -0,0 +1,104 @@
+﻿// CriticalSection.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <process.h>
+
+char* g_pszBuffer = NULL;
+CRITICAL_SECTION g_cs;
+
+void SetString(const char* pszData)
+{
+    ::EnterCriticalSection(&g_cs);
+    if (g_pszBuffer != NULL)
+    {
+        free(g_pszBuffer);
+        g_pszBuffer = (char*)malloc(64);
+        sprintf_s(g_pszBuffer, 64, "%s", pszData);
+    }
+    else
+    {
+        g_pszBuffer = (char*)malloc(64);
+        sprintf_s(g_pszBuffer, 64, "%s", pszData);
+    }
+    ::LeaveCriticalSection(&g_cs);
+}
+
+BOOL GetString(char* pszData)
+{
+    ::EnterCriticalSection(&g_cs);
+    if (pszData != NULL)
+    {
+        sprintf_s(pszData, 64, "%s", g_pszBuffer);
+        free(g_pszBuffer);
+        g_pszBuffer = NULL;
+
+        ::LeaveCriticalSection(&g_cs);
+        return TRUE;
+    }    
+
+    ::LeaveCriticalSection(&g_cs);
+    return FALSE;
+}
+
+UINT ThreadFunc1(LPVOID pParam)
+{
+    while (TRUE)
+    {
+        ::Sleep(1);
+        SetString("ThreadFunc1");
+    }
+    return 0;
+}
+
+UINT ThreadFunc2(LPVOID pParam)
+{
+    while (TRUE)
+    {
+        ::Sleep(1);
+        SetString("ThreadFunc2");
+    }
+    return 0;
+}
+
+int main()
+{
+    ::InitializeCriticalSection(&g_cs);
+
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunc1,
+        NULL,
+        0,
+        &nThreadId
+    );
+    ::CloseHandle(hThread);
+
+    hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunc2,
+        NULL,
+        0,
+        &nThreadId
+    );
+
+    char szBuffer[64];
+    for (int i = 0; i < 5; i++)
+    {
+        ::Sleep(500);
+
+        GetString(szBuffer);
+        puts(szBuffer);
+    }
+
+    WaitForSingleObject(hThread, INFINITE);
+    ::CloseHandle(hThread);
+
+    ::DeleteCriticalSection(&g_cs);
+    return 0;
+}
---
+++ windows-system-programing/thread/ThreadEventWait/ThreadEventWait/ThreadEventWait.cpp
@@ -0,0 +1,50 @@
+﻿// ThreadEventWait.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <conio.h>
+#include <process.h>
+#include <windows.h>
+
+UINT WINAPI ThreadFunction(LPVOID pParam)
+{
+    HANDLE hEvent = pParam;
+    std::cout << "ThreadFunction() - Begin" << std::endl;
+
+    _getch();
+    SetEvent(hEvent);
+    std::cout << "ThreadFunction() - SetEvent" << std::endl;
+    std::cout << "ThreadFunction() - End" << std::endl;
+    return 0;
+}
+
+int main()
+{
+    std::cout << "main() - Begin" << std::endl;
+    HANDLE hEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+
+    UINT nThreadId = 0;
+    HANDLE hThread = (HANDLE)::_beginthreadex(
+        NULL,
+        0,
+        ThreadFunction,
+        (LPVOID)hEvent,
+        0,
+        &nThreadId
+    );
+    if (hThread == NULL) {
+        std::cout << "ERROR: Failed to begin thread." << std::endl;
+        return 0;
+    }
+
+    if (::WaitForSingleObject(
+        hEvent, INFINITE) == WAIT_OBJECT_0)
+    {
+        std::cout << "main() - WaitForSingleObject() return" << std::endl;        
+    }
+
+    ::WaitForSingleObject(hThread, INFINITE);
+    std::cout << "main() - Thread End" << std::endl;
+    ::CloseHandle(hThread);
+    std::cout << "main() - End" << std::endl;
+}
