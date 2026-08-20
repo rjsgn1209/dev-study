a517ab98ba47b95272b0a95ce5d11348b47ba819
rjsgn1209 <huu1209@naver.com>
2026-08-20 23:17:31 +0900

    study: 파일 맵핑 및 메모리 추상화 학습

---
+++ windows-system-programming/src/memory/FileMappoing/FileMappoing.cpp
@@ -0,0 +1,48 @@
+﻿// FileMappoing.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	HANDLE hFile = ::CreateFile(
+		TEXT("C:\\Test\\MYFILE.TXT"),
+		GENERIC_READ | GENERIC_WRITE,
+		FILE_SHARE_READ,
+		NULL,
+		CREATE_ALWAYS,
+		FILE_ATTRIBUTE_NORMAL,
+		NULL
+	);
+	::SetFilePointer(hFile, 1024, NULL, FILE_BEGIN);
+
+	HANDLE hMap = ::CreateFileMapping(
+		hFile, NULL, PAGE_READWRITE, 0, 1024, NULL);
+	if (hMap == NULL)
+	{
+		wprintf(L"Failed to create file mapping obj [ERROR CODE: %d]\n",
+			::GetLastError());
+
+		::CloseHandle(hFile);
+		return 0;
+	}
+
+	char* pszMemory = (char*)::MapViewOfFile(
+		hMap, FILE_MAP_ALL_ACCESS, 0, 0, 1024
+	);
+	if (pszMemory == NULL)
+	{
+		::CloseHandle(hMap);
+		::CloseHandle(hFile);
+		return 0;
+	}
+
+	strcpy_s(pszMemory, 1024, "Hello, Memory Mapped File!");
+
+	::UnmapViewOfFile(pszMemory);
+	::CloseHandle(hMap);
+
+	::CloseHandle(hFile);
+}
